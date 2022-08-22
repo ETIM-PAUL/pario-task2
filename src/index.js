50bfe5b98d3bc24ca8237e40ff46fd46c226772e
ETIM-PAUL <etimpaul22@gmail.com>
2022-08-22 13:15:55 +0100

    added redux, tailwind and chakra ui

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
